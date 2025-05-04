65e7f5f092b5d20b985ddef5d4e68b49ad74fb34
vladiscripts <blagopoluchie12@gmail.com>
2025-05-05 01:04:15 +0300

    fix import

--- core/modules/raydium_py/config.py
+++ core/modules/raydium_py/config.py
@@ -4,7 +4,7 @@ from solders.keypair import Keypair
 import sys
 sys.path.append('/home/swell/git/meteora/arbitrage')
 sys.path.append('/home/ubuntu/solana/arbitrage')
-from core.config import PAYER_PRIVATE_KEY, VAULT_PRIVATE_KEY, OPERATOR_PRIVATE_KEY, RPC_ENDPOINT_LIST, RPC_ENDPOINT_LIST_ID, UNIT_BUDGET, UNIT_PRICE
+from config import PAYER_PRIVATE_KEY, VAULT_PRIVATE_KEY, OPERATOR_PRIVATE_KEY, RPC_ENDPOINT_LIST, RPC_ENDPOINT_LIST_ID, UNIT_BUDGET, UNIT_PRICE
 
 payer_keypair = Keypair.from_base58_string(OPERATOR_PRIVATE_KEY)
 arb_vault_keypair = Keypair.from_base58_string(VAULT_PRIVATE_KEY)
